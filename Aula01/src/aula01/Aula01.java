d14bcf11ef1843e6f385864014c1d9fa49779360
Joao Machado <joaojrmachado@gmail.com>
2019-09-03 20:15:58 -0300

    Trabalho finalizado

--- Aula01/src/aula01/Aula01.java
+++ Aula01/src/aula01/Aula01.java
@@ -17,7 +17,6 @@ public class Aula01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
         Scanner input = new Scanner(System.in);
         int op=-1;
         do{
@@ -25,16 +24,19 @@ public class Aula01 {
             op = input.nextInt();
             switch(op){
                 case 1:
-                    System.out.println("Opção 1 escolhida: "+Math.random());
+                    System.out.println("Opção 1 escolhida: "+(int) (Math.random() * 50 + 1));
                     break;
                 case 2:
-                    System.out.println("Opção 2 escolhida.");
+                    System.out.println("Opção 2 escolhida. \n Imprimindo nros Ímpares.");
+                    printNumbers("Ímpar");
                     break;
                 case 3:
-                    System.out.println("Opção 3 escolhida.");
+                    System.out.println("Opção 3 escolhida. \n Imprimindo nros Pares.");
+                    printNumbers("Par");
                     break;
                 case 4: 
-                    System.out.println("Opção 4 escolhida.");
+                    System.out.println("Opção 4 escolhida. Encerrando programa.");
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Opção Inválida, tente novamente.");
@@ -42,5 +44,21 @@ public class Aula01 {
                     break;
             }
         }while(op!=0);
+    }
+    
+    public static void printNumbers(String op){
+        if(op=="Par"){
+            for(int i=0; i<100; i++){
+                if(i%2==0){
+                    System.out.println("Nro: "+i);
+                }
+            }
+        } else{
+            for(int i=0; i<100; i++){
+                if(i%2!=0){
+                    System.out.println("Nro: "+i);
+                }
+            }
+        }
     }
 }
